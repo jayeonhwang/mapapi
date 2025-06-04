71b7e20f778e94db3f7253eaf6a98b5225ee9017
Jayeon Hwang <jayeon512@gmail.com>
2025-06-04 17:16:43 -0500

    import leaflet

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 17);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+
+      L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+    },
+
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
