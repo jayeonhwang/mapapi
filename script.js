9f5584566921fac152ffcfc4dfe8e344c8c025a4
Jayeon Hwang <jayeon512@gmail.com>
2025-07-15 15:13:12 -0500

    render workout

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -21,6 +21,13 @@ class Workout {
     this.duration = duration;
   }
 
+  _setDescription() {
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+  }
+
   click() {
     this.clicks++;
   }
@@ -32,10 +39,13 @@ class Running extends Workout {
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
+    this.calcPace();
+    this._setDescription();
+
   }
   calcPace() {
     this.pace = this.duration / this.distance;
-    return this.pace
+    return this.pace;
   }
 }
 
@@ -44,6 +54,9 @@ class Cycling extends Workout {
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
+    this.calcSpeed();
+    this._setDescription();
+
   }
   calcSpeed() {
     this.speed = this.duration / (this.duration / 60);
@@ -167,7 +180,9 @@ class App {
     // this.#workouts.push(workout);
 
     // Render workout on map as marker
-    this.renderWorkoutMarker(workout)
+    this._renderWorkoutMarker(workout);
+    this._renderWorkout(workout);
+    this._hideForm(workout);
 
   }
 
@@ -178,7 +193,8 @@ class App {
 
   // // Set local storage to all workouts
   // this._setLocalStorage();
-  renderWorkoutMarker(workout) {
+
+  _renderWorkoutMarker(workout) {
     L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
@@ -190,10 +206,61 @@ class App {
           className: `${workout.type}-popup`
         })
       )
-      .setPopupContent('workout')
+      .setPopupContent(`${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'}${workout.description}`)
       .openPopup();
 
   }
+
+  _renderWorkout(workout) {
+    let html = `
+          <li class="workout workout--${workout.type}" data-id="${workout.id}">
+          <h2 class="workout__title">${workout.description}</h2>
+          <div class="workout__details">
+            <span class="workout__icon">${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} </span>
+      <span class="workout__value" > ${workout.distance}</span >
+        <span class="workout__unit">km</span>
+      </div >
+      <div class="workout__details">
+        <span class="workout__icon">⏱</span>
+        <span class="workout__value">${workout.duration}</span>
+        <span class="workout__unit">min</span>
+      </div>
+    `;
+
+
+    if (workout.type === `running`) {
+      html += `
+          <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value"> ${workout.pace.toFixed(1)}</span>
+            <span class="workout__unit">min/km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">🦶🏼</span>
+            <span class="workout__value">${workout.cadence}</span>
+            <span class="workout__unit">spm</span>
+          </div>
+        </li>`;
+    }
+    if (workout.type === `cycling`) {
+      html += `
+          <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">${workout.elevationGain}</span>
+            <span class="workout__unit">m</span>
+          </div>
+        </li>`;
+
+    }
+    form.insertAdjacentHTML('afterend', html);
+
+  }
+
 }
 
 const app = new App();
